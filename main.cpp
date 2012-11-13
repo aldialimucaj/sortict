316f3aac1d57847ca3571a730567cadfed8eb9bd
Aldi Alimucaj <aldi@outlook.com>
2012-11-13 21:20:40 +0100

    + first release

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char** argv) {
     desc.add_options()
             ("help", "Print this page")
             ("source,s", value<string > (), "Source Folder(s) where to look for the media data")
-            ("destination,d", value<string > (), "Destination Folder where to reallocate the files")
+            ("destination,d", value<string > (), "Destination Folder where to reallocate the files. If no destination is set, the source folder will be set as destination")
             ("interleave,i", value<int>(), "the amount of sub-folder to create within the sorted folder. Example --interleave=1 would generate a structure like $DEST_FOLDER/A/AA")
             ("no-rest,n", "Don't move NON alphanumeric (A-Z) files")
             ;
@@ -42,7 +42,7 @@ int main(int argc, char** argv) {
     store(parse_command_line(argc, argv, desc), vm);
     notify(vm);
 
-    if (vm.count("help")) {
+    if (vm.count("help") || vm.size() == 0) {
         cout << desc << "\n";
         return 0;
     }
@@ -50,11 +50,18 @@ int main(int argc, char** argv) {
     if (vm.count("source")) {
         _srcPath = vm["source"].as<string > ();
         sorter.setSrcPath(_srcPath);
+    }else {
+        cout << "The source folder is required!" << endl;
+        cout << desc << endl;
+        return 0;
     }
 
     if (vm.count("destination")) {
         _dstPath = vm["destination"].as<string > ();
         sorter.setDstPath(_dstPath);
+    }else if(!vm.count("destination") && vm.count("source")){
+        _srcPath = vm["source"].as<string > ();
+        sorter.setDstPath(_srcPath);
     }
 
     if (vm.count("interleave")) {
@@ -73,12 +80,3 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-
-
-
-//    else {
-//        _srcPath = iosorter.getPath("Source Path");
-//    }
-//    else {
-//        _dstPath = iosorter.getPath("Destination Path");
-//    }
--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -39,13 +39,6 @@ OBJECTFILES= \
 	${OBJECTDIR}/IOSorter.o \
 	${OBJECTDIR}/Sortit.o
 
-# Test Directory
-TESTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tests
-
-# Test Files
-TESTFILES= \
-	${TESTDIR}/TestFiles/f1 \
-	${TESTDIR}/TestFiles/f2
 
 # C Compiler Flags
 CFLAGS=
@@ -74,98 +67,20 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/sortit: ${OBJECTFILES}
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
+	$(COMPILE.cc) -O2 -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
 
 ${OBJECTDIR}/IOSorter.o: IOSorter.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/IOSorter.o IOSorter.cpp
+	$(COMPILE.cc) -O2 -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/IOSorter.o IOSorter.cpp
 
 ${OBJECTDIR}/Sortit.o: Sortit.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
-	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/Sortit.o Sortit.cpp
+	$(COMPILE.cc) -O2 -std=c++11 -MMD -MP -MF $@.d -o ${OBJECTDIR}/Sortit.o Sortit.cpp
 
 # Subprojects
 .build-subprojects:
-
-# Build Test Targets
-.build-tests-conf: .build-conf ${TESTFILES}
-${TESTDIR}/TestFiles/f1: ${TESTDIR}/tests/newsimpletest.o ${OBJECTFILES:%.o=%_nomain.o}
-	${MKDIR} -p ${TESTDIR}/TestFiles
-	${LINK.cc}   -o ${TESTDIR}/TestFiles/f1 $^ ${LDLIBSOPTIONS} 
-
-${TESTDIR}/TestFiles/f2: ${TESTDIR}/tests/newtestclass.o ${TESTDIR}/tests/newtestrunner.o ${OBJECTFILES:%.o=%_nomain.o}
-	${MKDIR} -p ${TESTDIR}/TestFiles
-	${LINK.cc}   -o ${TESTDIR}/TestFiles/f2 $^ ${LDLIBSOPTIONS} -lcppunit 
-
-
-${TESTDIR}/tests/newsimpletest.o: tests/newsimpletest.cpp 
-	${MKDIR} -p ${TESTDIR}/tests
-	${RM} $@.d
-	$(COMPILE.cc) -O2 -I. -MMD -MP -MF $@.d -o ${TESTDIR}/tests/newsimpletest.o tests/newsimpletest.cpp
-
-
-${TESTDIR}/tests/newtestclass.o: tests/newtestclass.cpp 
-	${MKDIR} -p ${TESTDIR}/tests
-	${RM} $@.d
-	$(COMPILE.cc) -O2 -I. -MMD -MP -MF $@.d -o ${TESTDIR}/tests/newtestclass.o tests/newtestclass.cpp
-
-
-${TESTDIR}/tests/newtestrunner.o: tests/newtestrunner.cpp 
-	${MKDIR} -p ${TESTDIR}/tests
-	${RM} $@.d
-	$(COMPILE.cc) -O2 -I. -MMD -MP -MF $@.d -o ${TESTDIR}/tests/newtestrunner.o tests/newtestrunner.cpp
-
-
-${OBJECTDIR}/main_nomain.o: ${OBJECTDIR}/main.o main.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/main.o`; \
-	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
-	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.cc) -O2 -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/main_nomain.o main.cpp;\
-	else  \
-	    ${CP} ${OBJECTDIR}/main.o ${OBJECTDIR}/main_nomain.o;\
-	fi
-
-${OBJECTDIR}/IOSorter_nomain.o: ${OBJECTDIR}/IOSorter.o IOSorter.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/IOSorter.o`; \
-	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
-	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.cc) -O2 -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/IOSorter_nomain.o IOSorter.cpp;\
-	else  \
-	    ${CP} ${OBJECTDIR}/IOSorter.o ${OBJECTDIR}/IOSorter_nomain.o;\
-	fi
-
-${OBJECTDIR}/Sortit_nomain.o: ${OBJECTDIR}/Sortit.o Sortit.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	@NMOUTPUT=`${NM} ${OBJECTDIR}/Sortit.o`; \
-	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
-	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
-	then  \
-	    ${RM} $@.d;\
-	    $(COMPILE.cc) -O2 -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/Sortit_nomain.o Sortit.cpp;\
-	else  \
-	    ${CP} ${OBJECTDIR}/Sortit.o ${OBJECTDIR}/Sortit_nomain.o;\
-	fi
-
-# Run Test Targets
-.test-conf:
-	@if [ "${TEST}" = "" ]; \
-	then  \
-	    ${TESTDIR}/TestFiles/f1 || true; \
-	    ${TESTDIR}/TestFiles/f2 || true; \
-	else  \
-	    ./${TEST} || true; \
-	fi
 
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
